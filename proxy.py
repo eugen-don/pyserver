005bf0002235482bbd64976bbfa55679b352a0d2
billypu <yinpsoft@vip.qq.com>
2015-10-09 10:18:57 +0800

    add pygame ex

--- proxy.py
+++ proxy.py
@@ -8,7 +8,7 @@ from async_server import PyAsyncServer
 
 if __name__ == "__main__" :
     svr = PyAsyncServer(("", 9100))
-    svr.add_session_config(name="backendsvr1", ip="localhost", port=9000)
-    svr.add_session_config(name="backendsvr1", ip="localhost", port=9001)
-    svr.add_session_config(name="backendsvr1", ip="localhost", port=9002)
+    svr.add_session_config(name="login_check_svr", ip="localhost", port=9000)
+    svr.add_session_config(name="game_logic_svr", ip="localhost", port=9001)
+    # svr.add_session_config(name="backendsvr1", ip="localhost", port=9002)
     svr.run()
